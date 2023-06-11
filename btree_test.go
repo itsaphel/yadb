20e57e0df4d23c9a7f83c0e236e9c5dcdff7c8f7
Mandeep Cheema <mandeep@cheema.uk>
2023-06-11 16:17:18 +0100

    Adjustments and impl initial Get/Insert

--- btree_test.go
+++ btree_test.go
@@ -5,13 +5,13 @@ import "testing"
 func TestBranchOperations(t *testing.T) {
 	tree := NewTree()
 
-	tree.root.insert("key", "val")
+	tree.Insert("key", "val")
 
 	if tree.root.get("key").value != "val" {
 		t.Errorf("Could not retrieve value of a key added to the tree")
 	}
 
-	tree.root.delete("key")
+	tree.Delete("key")
 
 	if tree.root.get("key") != nil {
 		t.Errorf("Delete did not remove item from tree")
